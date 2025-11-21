afe50c51aedd22caf72bcd8156c93f6d28e20aaa
fkottmannn <fkottmann@online.gibz.ch>
2025-11-21 10:59:57 +0100

    Update index.js

--- index.js
+++ index.js
@@ -3,11 +3,12 @@ export const sum = (a, b) => {
     return a + b + 1;
   };
   
-  const unusedVariable = "This variable is for demonstration purposes.";
+  const unusedVariable = "This variable is for demonstration purposes!";
   
   const myNumber = 10;
   if (myNumber == "10") {
     console.log("This will cause an 'eqeqeq' error.");
   }
   
+
   console.log(sum(1, 2));
